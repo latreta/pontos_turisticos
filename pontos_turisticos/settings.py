fda977fcd504eb4f4b1b162dc97da1567768458b
latreta <yveshenr@gmail.com>
2020-07-09 02:53:26 -0300

    fixing the url

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 MEDIA_ROOT = 'imagens'
 MEDIA_URL = '/media/'
 
