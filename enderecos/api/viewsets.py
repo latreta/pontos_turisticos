1467d9aeffc69ce1a74247151b50b50f42690003
latreta <yveshenr@gmail.com>
2020-07-22 06:29:47 -0300

    removing roles file on dif app

--- enderecos/api/viewsets.py
+++ enderecos/api/viewsets.py
@@ -1,8 +1,4 @@
-from django.http import HttpResponse
 from rest_framework.viewsets import ModelViewSet
-from rest_framework_simplejwt.state import User
-from rolepermissions.checkers import has_permission
-from rolepermissions.roles import assign_role
 
 from enderecos.api.serializers import EnderecoSerializer
 from enderecos.models import Endereco
